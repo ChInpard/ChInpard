2d1fcc0ad5be6264c88ebc0357283dff65dd17c7
Philip <aloha@saltmine.io>
2024-08-16 11:47:23 +0900

    useUpdateCheckedState useEffect 무한루프 버그 수정

--- src/hooks/useUpdateCheckedState.js
+++ src/hooks/useUpdateCheckedState.js
@@ -7,8 +7,14 @@ export function useUpdateCheckedState(isChecked, todo, list, setList) {
         const updatedList = list.map(item => 
             item.id === todo.id ? { ...item, checked: isChecked } : item
         );
-        setList(updatedList);
-        localStorage.setItem('written', JSON.stringify(updatedList));
-    }, [isChecked, list, setList, todo.id]);
+        
+        const isListUpdated = JSON.stringify(updatedList) !== JSON.stringify(list);
+        
+        if (isListUpdated) {
+            setList(updatedList);
+            localStorage.setItem('written', JSON.stringify(updatedList));
+        }
+        
+    }, [isChecked, todo.id]);
 
 }
